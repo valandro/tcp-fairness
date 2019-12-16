06057efbab84586462eada0a2299ce32b4e435e9
Lucas <lvalandrorocha@hotmail.com>
2019-12-16 19:15:40 -0300

    Add server/client code for traffic measurement

--- client/src/Client.java
+++ client/src/Client.java
@@ -1,14 +1,38 @@
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args) {
-        try {
-            Socket socket = new Socket("localhost", Integer.valueOf(args[0]));
-            System.out.println("Socket connected!");
-            socket.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+
+    private static Runnable clientConn(final String host, final int port) {
+        return () -> {
+            final long times = 10000;
+            final int bufferSize = 1024;
+            try {
+                Socket socket = new Socket(host, port);
+                System.out.println("Client on port " + socket.getLocalPort() + " connected on server " + host + " on port " + port);
+                for (int i = 0; i < times; i++) {
+                    DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+                    dOut.writeInt(bufferSize);
+                    dOut.write(new byte[bufferSize]);
+                }
+                System.out.println("Client " + socket.getLocalPort() + " closed connection!");
+                socket.close();
+            } catch (IOException ex) {
+                System.out.println("Socket error!");
+                ex.printStackTrace();
+            }
+        };
+    }
+
+    public static void main(String[] args) throws Exception {
+        String host = args[0];
+        int port = Integer.valueOf(args[1]);
+        System.out.println("Clients starting...!");
+        new Thread(clientConn(host, port)).start();
+        Thread.sleep(5000);
+        new Thread(clientConn(host, port)).start();
+        Thread.sleep(5000);
+        new Thread(clientConn(host, port)).start();
     }
 }
--- server/src/Server.java
+++ server/src/Server.java
@@ -1,18 +1,104 @@
-import java.io.IOException;
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(Integer.valueOf(args[0]));
-            System.out.println("Server listening on port " + args[0]);
-            Socket socket = serverSocket.accept();
-            System.out.println("Server connected!");
-            socket.close();
-            System.out.println("Server disconnected!");
-        } catch (IOException ex) {
+            final int port = Integer.valueOf(args[0]);
+            final int fixedNumberOfThreads = 3;
+
+            ServerSocket tcpSocket = new ServerSocket(port);
+
+            System.out.println("-------- SERVER LISTENING ON PORT -------");
+            System.out.println("                " + port);
+            System.out.println("-----------------------------------------");
+
+            int threadsRunning = 0;
+            boolean hasData = true;
+
+            while (hasData) {
+
+                Socket socket = tcpSocket.accept();
+
+                System.out.println("-------- SERVER LISTENING ON PORT -----");
+                System.out.println("                " + socket.getPort());
+                System.out.println("---------------------------------------");
+                new ReceiveDataThread(socket).start();
+                // @TODO: Count the number of running threads
+                threadsRunning++;
+                if (threadsRunning == fixedNumberOfThreads) {
+                    hasData = false;
+                }
+            }
+            System.out.println("-------- SERVER CLOSING TCP CONN ------");
+            tcpSocket.close();
+        } catch (Exception ex) {
             ex.printStackTrace();
+        }
+    }
+}
+
+class ReceiveDataThread extends Thread {
+    private Socket socket;
+
+    ReceiveDataThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        final int megaBits = 1024 * 1024;
+        int totalRecv = 0;
+        long start = System.currentTimeMillis();
+
+        int dataLength = 0;
+        boolean hasData = true;
+
+        while(hasData) {
+
+            try {
+                DataInputStream dis = new DataInputStream(socket.getInputStream());
+                dataLength = dis.readInt();
+            } catch (Exception eof) {
+                hasData = false;
+                try {
+                    System.out.println("------ SERVER CLOSING CONN ON PORT ----");
+                    System.out.println("                " + socket.getPort());
+                    System.out.println("---------------------------------------");
+                    socket.close();
+                } catch (IOException ex) {
+                    System.out.println("Error closing socket with client " + socket.getPort());
+                    System.exit(0);
+                }
+            }
+            // Has data on recv buffer
+            if (dataLength >= 0) {
+                totalRecv += dataLength;
+
+                long stop = System.currentTimeMillis();
+
+                long time = stop - start;
+
+                // Wait 1s to measure bandwith
+                if (time >= 1000) {
+                    final double bandWith = (totalRecv * 8) / ((time / 1000.0) * megaBits);
+                    final BigDecimal bandWithPrecision =
+                            BigDecimal.valueOf(bandWith).setScale(2, RoundingMode.HALF_EVEN);
+                    System.out.println("---- C "+ socket.getPort() + " BANDWIDTH " + bandWithPrecision +" MBITS/S ----");
+                    // Reset total receive and time chronometer
+                    totalRecv = 0;
+                    start = System.currentTimeMillis();
+                }
+            } else {
+                try {
+                    socket.close();
+                } catch (IOException ex) {
+                    return;
+                }
+            }
         }
     }
 }
